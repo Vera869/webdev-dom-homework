ef76a3f3a5343bc8aa187bc45217a7120693192a
Vera869 <verea8910@gmail.com>
2023-10-25 14:48:54 +0700

    переименование, авторизация, токен

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { getFitchTodos, postFetchTodo } from "./modules/fitchTodos.js"
 import { commentsRender, renderCommentInGet } from "./modules/renderTodos.js"
-import { errorProcessingGet, errorProcessingPost, checkButtonClick } from "./modules/checkFitchTodos.js"
+import { errorProcessingGet, errorProcessingPost, checkButtonClick } from "./modules/check.js"
 import { islikedComment, likeClick, renderAnswer } from "./modules/likesAndAnswer.js"
 import { newDateElement } from "./modules/other.js"
 
@@ -20,58 +20,58 @@ startLoader.style.display = "flex";
 let comments = [];
 const getFetch = () => {
    getFitchTodos()
-  .then((responseLoader) => {
-      startLoader.style.display = "none";
-      return responseLoader;
-  }).then((responseData) => {
-   renderCommentInGet({responseData, comments, newDateElement, rendercomments});
-   }).catch((error) => {
-      errorProcessingGet(error)
-   });
+      .then((responseLoader) => {
+         startLoader.style.display = "none";
+         return responseLoader;
+      }).then((responseData) => {
+         renderCommentInGet({ responseData, comments, newDateElement, rendercomments });
+      }).catch((error) => {
+         errorProcessingGet(error)
+      });
 };
 getFetch();
 
 const rendercomments = (appComments = []) => {
-  commentsRender ({comments: appComments, commentBoxElement})
-  addLikeEventListeners();
-  answerComment()
+   commentsRender({ comments: appComments, commentBoxElement })
+   addLikeEventListeners();
+   answerComment()
 }
 rendercomments();
 
 const postFetch = () => {
-   postFetchTodo({formNameElement, formTextElement})
-   .then((responseData) => {
-      return getFetch();
-   }).then(() => {
-      commentLoader.style.display = "none";
-      addForm.style.display = "flex";
-      formNameElement.value = "";
-      formTextElement.value = "";
-   }).catch((error) => {
-      errorProcessingPost({error, addForm, commentLoader, getFetch})
-    });
+   postFetchTodo({ formNameElement, formTextElement })
+      .then((responseData) => {
+         return getFetch();
+      }).then(() => {
+         commentLoader.style.display = "none";
+         addForm.style.display = "flex";
+         formNameElement.value = "";
+         formTextElement.value = "";
+      }).catch((error) => {
+         errorProcessingPost({ error, addForm, commentLoader, getFetch })
+      });
 }
 buttonElement.addEventListener('click', (event) => {
-  checkButtonClick({formNameElement, formTextElement, commentLoader, addForm, postFetch});
-  event.stopPropagation();
-  rendercomments();
+   checkButtonClick({ formNameElement, formTextElement, commentLoader, addForm, postFetch });
+   event.stopPropagation();
+   rendercomments();
 
 });
-function addlike(index) {  
-  const likebuttons = document.querySelectorAll('.like-button');
-  const likebutton = likebuttons[index];
-  const comment = comments[index];
-  islikedComment({comment, islike, rendercomments});
-} 
+function addlike(index) {
+   const likebuttons = document.querySelectorAll('.like-button');
+   const likebutton = likebuttons[index];
+   const comment = comments[index];
+   islikedComment({ comment, islike, rendercomments });
+}
 function addLikeEventListeners() {
-  const likebuttons = document.querySelectorAll('.like-button');
-  likeClick({likebuttons, addlike});
+   const likebuttons = document.querySelectorAll('.like-button');
+   likeClick({ likebuttons, addlike });
 }
 function answerComment() {
-  const commentsElement = document.querySelectorAll('.comment');
-  const commentName = document.querySelectorAll('.name');
-  const commentText = document.querySelectorAll('.comment-text');
-  renderAnswer({commentsElement, formTextElement, commentText, commentName});
+   const commentsElement = document.querySelectorAll('.comment');
+   const commentName = document.querySelectorAll('.name');
+   const commentText = document.querySelectorAll('.comment-text');
+   renderAnswer({ commentsElement, formTextElement, commentText, commentName });
 };
 addLikeEventListeners();
 rendercomments();
--- modules/fitchTodos.js
+++ modules/fitchTodos.js
@@ -1,31 +1,39 @@
-import { checkFitch } from "./checkFitchTodos.js"
+import { checkFitch } from "./check.js"
+
+const host = "https://wedev-api.sky.pro/api/v2/vera-Bu/comments";
+let token = "Bearer bgc0b8awbwas6g5g5k5o5s5w606g37w3cc3bo3b83k39s3co3c83c03ck";
 
 export function getFitchTodos() {
    return fetch("https://wedev-api.sky.pro/api/v1/vera-Bug/comments", {
-   method: "GET"
- }).then((response) => {
-   checkFitch(response);
-   return response.json();
-});
+      method: "GET",
+      headers: {
+         Authorization: token,
+      },
+   }).then((response) => {
+      checkFitch(response);
+      return response.json();
+   });
 }
-
-export function postFetchTodo({formNameElement, formTextElement}) {
+export function postFetchTodo({ formNameElement, formTextElement }) {
    return fetch("https://wedev-api.sky.pro/api/v1/vera-Bug/comments", {
-   method: "POST",
-   body: JSON.stringify({
-      name: formNameElement.value
-      .replaceAll("&", "&amp;")
-      .replaceAll("<", "&lt;")
-      .replaceAll(">", "&gt;")
-      .replaceAll('"', "&quot;"),
-      text: formTextElement.value
-      .replaceAll("&", "&amp;")
-      .replaceAll("<", "&lt;")
-      .replaceAll(">", "&gt;")
-      .replaceAll('"', "&quot;"),
-      //forceError: true,
-    })
-  }).then((response) => {
+      method: "POST",
+      body: JSON.stringify({
+         name: formNameElement.value
+            .replaceAll("&", "&amp;")
+            .replaceAll("<", "&lt;")
+            .replaceAll(">", "&gt;")
+            .replaceAll('"', "&quot;"),
+         text: formTextElement.value
+            .replaceAll("&", "&amp;")
+            .replaceAll("<", "&lt;")
+            .replaceAll(">", "&gt;")
+            .replaceAll('"', "&quot;"),
+         headers: {
+            Authorization: token,
+         },
+         //forceError: true,
+      })
+   }).then((response) => {
       checkFitch(response)
       return response.json()
    })
