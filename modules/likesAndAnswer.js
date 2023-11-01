49c00ef02b9895b7083ff717380827da8dd9ab49
Vera869 <verea8910@gmail.com>
2023-11-01 12:49:06 +0700

    добавлен модуль с лайками

---
+++ modules/likesAndAnswer.js
@@ -0,0 +1,29 @@
+export function islikedComment({comment, islike, rendercomments}) {
+   if(!comment.isLike) {
+    comment.countLike += 1;
+    comment.isLike = true; 
+    rendercomments();
+  } else {
+    comment.countLike -= 1;
+    comment.isLike = false;
+    rendercomments();
+  }
+ }
+ export function likeClick({likebuttons, addlike}) {
+    likebuttons.forEach((likebutton, index) => {
+       likebutton.addEventListener('click', (event) => {
+         event.stopPropagation();
+         addlike(index);
+       });
+     });
+ }
+ export function renderAnswer({commentsElement, formTextElement, commentText, commentName}) {
+    const commentBody = document.querySelector('.comment-body');
+    commentsElement.forEach((commentBody, index) => {
+      commentBody.addEventListener('click', () => {
+        formTextElement.value = `${commentText[index].textContent} 
+          ${commentName[index].textContent}`;
+      });
+    })
+ }
+ 
